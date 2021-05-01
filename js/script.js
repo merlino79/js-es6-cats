73b9255cd3af9e2782a53be77e906a96b8c0a04a
Merlino79 <alessandro.ciancio@hotmail.com>
2021-05-01 12:06:28 +0200

    operatore ternario

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 // definire array ogg che rappresenta un gatto carat nome, età,colore e sesso 
 //treamite la funzione forEach stampare in pagina tutti i gattini, ciascuno con il proprio colore ed il proprio nome
 //... mile stone 2 
+//dividere i gatti in due contenitori distinti in base al sesso e agg un fiocco rosa o blu in base all gender
+//il colore deve essere più scuro se è vecchio  o più chiaro se giovane 
+
 //... mile stone 3 
 
 $(function() {
@@ -10,13 +13,13 @@ $(function() {
             name: 'astra',
             age: 1.5,
             color: '#AB6943',
-            gender: 'male',
+            gender: 'female',
         },
         {
             name: 'plutone',
             age: 3,
             color: '#884874',
-            gender: 'female',
+            gender: 'male',
         },
         {
             name: 'marte',
@@ -45,7 +48,7 @@ $(function() {
         {
             name: 'tigre',
             age: 1.5,
-            color: '#C55873',
+            color: '#C55873 ',
             gender: 'male',
         },
 
@@ -63,6 +66,19 @@ $(function() {
         //console.log(cat);
         //console.log(listGenerator(cat.color, cat.name))
         $('#mailes-1').append(listGenerator(cat.color, cat.name));
+    })
+
+    //mailes 2
+
+    //map per il clone 
+    const pink = "#ff00e6";
+    const blue = "#0084ff";
+
+    const newCats = cats.map((cat) => {
+        //opratore termario per le condizioni if(?) se (:)
+        let color = (cat.gender === 'female') ? pink : blue;
+        console.log(color);
+
     })
 
 
@@ -83,7 +99,7 @@ $(function() {
 });
 //funzione per stama
 function listGenerator(catColor, name) {
-    console.log(catColor, name);
+    //console.log(catColor, name);
     let html = `
     <li>
          <i class="fas fa-cat" style="color:${catColor}"></i>
